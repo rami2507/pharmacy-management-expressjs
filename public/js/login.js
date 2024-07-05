9b77e77d8be5f99a93771fc85d4c492ca7879886
Rami Laloui <lalouirami34@gmail.com>
2024-07-05 03:35:12 +0100

    login.js edited

--- public/js/login.js
+++ public/js/login.js
@@ -1,43 +1,34 @@
 const loginPharmacist = async (email, password) => {
   try {
-    const res = await fetch("/api/v1/users/login", {
+    const res = await axios({
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-      timeout: 30000, // Note: fetch does not support timeout natively
+      url: "/api/v1/users/login",
+      data: { email, password },
     });
-    if (res.ok) {
-      alert("Hi, You are logged in successfully");
+    if (
+      res.data.status === "success" &&
+      res.data.data.user.role === "pharmacist"
+    ) {
+      alert("Hi Pharmacist, You are logged in successfully");
       window.setTimeout(() => {
         location.assign("/pharmacistDashboard");
       }, 1500);
     }
-    // if (
-    //   res.data.status === "success" &&
-    //   res.data.data.user.role === "pharmacist"
-    // ) {
-    //   alert("Hi Pharmacist, You are logged in successfully");
-    //   window.setTimeout(() => {
-    //     location.assign("/pharmacistDashboard");
-    //   }, 1500);
-    // }
-    // if (
-    //   res.data.status === "success" &&
-    //   res.data.data.user.role === "manager"
-    // ) {
-    //   alert("Hi Manager, You are logged in successfully");
-    //   window.setTimeout(() => {
-    //     location.assign("/managerDashboard");
-    //   }, 1500);
-    // }
-    // if (res.data.status === "success" && res.data.data.user.role === "seller") {
-    //   alert("Hi Seller, You are logged in successfully");
-    //   window.setTimeout(() => {
-    //     location.assign("/sellerDashboard");
-    //   }, 1500);
-    // }
+    if (
+      res.data.status === "success" &&
+      res.data.data.user.role === "manager"
+    ) {
+      alert("Hi Manager, You are logged in successfully");
+      window.setTimeout(() => {
+        location.assign("/managerDashboard");
+      }, 1500);
+    }
+    if (res.data.status === "success" && res.data.data.user.role === "seller") {
+      alert("Hi Seller, You are logged in successfully");
+      window.setTimeout(() => {
+        location.assign("/sellerDashboard");
+      }, 1500);
+    }
   } catch (err) {
     console.log(err);
     alert(err.response.data.message);
