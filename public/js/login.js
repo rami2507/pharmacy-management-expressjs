6d14de6088f157421d2ccb67c80e1dcdb09c6002
Rami Laloui <lalouirami34@gmail.com>
2024-07-05 02:57:39 +0100

    Edited login.js file

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 const loginPharmacist = async (email, password) => {
   try {
-    const res = await fetch("http://127.0.0.1:3000/api/v1/users/login", {
+    const res = await fetch("/api/v1/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
